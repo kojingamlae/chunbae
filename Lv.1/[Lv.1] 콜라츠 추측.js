89d41533835a84cdc12c797949466526be8904e4
SEOKKAMONI <sj01048260279@gmail.com>
2023-05-24 11:52:42 +0900

    ADD :: [Lv.1] 콜라츠 추측

---
+++ Lv.1/[Lv.1] 콜라츠 추측.js
@@ -0,0 +1,21 @@
+function solution(num) {
+  let count = 0;
+
+  if (num === 1) {
+    return 0;
+  }
+
+  while (num !== 1) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else if (num % 2 === 1) {
+      num *= 3;
+      num += 1;
+    }
+    if (count >= 500) {
+      return -1;
+    }
+    count++;
+  }
+  return count;
+}
