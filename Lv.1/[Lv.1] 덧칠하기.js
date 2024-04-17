1ee760211e9e47839ce51f70f09af1c219b1220a
김석진 <sj01048260279@gmail.com>
2024-04-17 19:57:12 +0900

    REFACTOR :: [Lv.1] 덧칠하기

--- Lv.1/[Lv.1] 덧칠하기.js
+++ Lv.1/[Lv.1] 덧칠하기.js
@@ -1,14 +1,10 @@
 function solution(n, m, section) {
     let answer = 0;
-    let painted = 0;
-    
-    for(let wall of section) {
-        if(painted >= wall) {
-            continue;
+    for(let i = 1; i < n+1; i++) {
+        if(section.includes(i)) {
+            i = i + m - 1;
+            answer++;
         }
-        answer++;
-        painted = wall + m - 1;
     }
-    
     return answer;
 }
